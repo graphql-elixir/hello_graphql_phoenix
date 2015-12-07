bb3e55269ee8cba9d1ea0eb4be057d40b7c70784
Josh Price <josh@canoniq.com>
2015-12-07 18:14:12 +1100

    Fix broken page spec

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule HelloGraphQL.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to GraphQL!"
   end
 end
