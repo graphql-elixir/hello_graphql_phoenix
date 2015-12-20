c4bdfd9c80503ebb57eddf2a8ad20fd08d6a2213
Josh Price <josh@canoniq.com>
2015-12-20 16:26:11 -0500

    Migrate to Phoenix 1.1

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule HelloGraphQL.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.3"},
+    [{:phoenix, "~> 1.1"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
