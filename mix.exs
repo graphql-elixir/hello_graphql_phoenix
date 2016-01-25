f0d9d1d89e49bd0b98a07374a44012eedbc15fb1
Josh Price <josh@canoniq.com>
2016-01-26 00:55:09 +1100

    Bump to plug_graphql 0.1.4 for GraphiQL multiline support

--- mix.exs
+++ mix.exs
@@ -36,6 +36,6 @@ defmodule HelloGraphQL.Mixfile do
      {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:plug_graphql, "~> 0.1.3"}]
+     {:plug_graphql, "~> 0.1.4"}]
   end
 end
