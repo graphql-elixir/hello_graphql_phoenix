d93abe0b52a008cd5ff607108460661645f0b274
Josh Price <josh@canoniq.com>
2016-02-10 17:36:05 +1100

    Remove SSL redirect so ALIAS will mask heroku app

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,6 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hello_graphql, HelloGraphQL.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "hello-graphql-phoenix.herokuapp.com", port: 443],
-    force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
